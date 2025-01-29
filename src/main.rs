87980aca171c469045e638c16f8a429496c2c684
blinry <mail@blinry.org>
2025-01-29 17:01:51 +0100

    Better names for stuff

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::marker::PhantomData;
 //                    \_/___________________/
 //
 
-trait Pipe<FrontInput, BackInput, BackOutput, FrontOutput> {
+trait Pipe<FrontInput, FrontOutput, BackInput, BackOutput> {
     fn handle_front_input(&mut self, message: FrontInput);
     fn handle_back_input(&mut self, message: BackInput);
     fn poll_front_output(&mut self) -> Option<FrontOutput>;
@@ -30,8 +30,8 @@ trait Pipe<FrontInput, BackInput, BackOutput, FrontOutput> {
 
 struct Glue<A, B, FrontInput, BackInput, BToA, BackOutput, FrontOutput, AToB>
 where
-    A: Pipe<FrontInput, BToA, AToB, FrontOutput>,
-    B: Pipe<AToB, BackInput, BackOutput, BToA>,
+    A: Pipe<FrontInput, FrontOutput, BToA, AToB>,
+    B: Pipe<AToB, BToA, BackInput, BackOutput>,
 {
     a: A,
     b: B,
@@ -42,8 +42,8 @@ where
 impl<A, B, FrontInput, BackInput, BToA, BackOutput, FrontOutput, AToB>
     Glue<A, B, FrontInput, BackInput, BToA, BackOutput, FrontOutput, AToB>
 where
-    A: Pipe<FrontInput, BToA, AToB, FrontOutput>,
-    B: Pipe<AToB, BackInput, BackOutput, BToA>,
+    A: Pipe<FrontInput, FrontOutput, BToA, AToB>,
+    B: Pipe<AToB, BToA, BackInput, BackOutput>,
 {
     fn new(a: A, b: B) -> Self {
         Self {
@@ -56,11 +56,11 @@ where
 
 // When driving a glued-together pipe, forward messages between the two sub-pipes.
 impl<A, B, FrontInput, BackInput, BToA, BackOutput, FrontOutput, AToB>
-    Pipe<FrontInput, BackInput, BackOutput, FrontOutput>
+    Pipe<FrontInput, FrontOutput, BackInput, BackOutput>
     for Glue<A, B, FrontInput, BackInput, BToA, BackOutput, FrontOutput, AToB>
 where
-    A: Pipe<FrontInput, BToA, AToB, FrontOutput>,
-    B: Pipe<AToB, BackInput, BackOutput, BToA>,
+    A: Pipe<FrontInput, FrontOutput, BToA, AToB>,
+    B: Pipe<AToB, BToA, BackInput, BackOutput>,
 {
     fn handle_front_input(&mut self, message: FrontInput) {
         self.a.handle_front_input(message);
@@ -83,30 +83,35 @@ where
     }
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+use std::collections::VecDeque;
+use std::io::{Read, Write};
+
 #[derive(Default)]
 struct BytesToLinesPipe {
-    input_from_io: Vec<u8>,
-    input_to_io: Vec<String>,
+    front_input: VecDeque<u8>,
+    back_input: VecDeque<String>,
 }
 
-impl Pipe<Vec<u8>, String, String, Vec<u8>> for BytesToLinesPipe {
-    fn handle_front_input(&mut self, message: Vec<u8>) {
-        self.input_from_io.extend(message);
+impl Pipe<Vec<u8>, Vec<u8>, String, String> for BytesToLinesPipe {
+    fn handle_front_input(&mut self, bytes: Vec<u8>) {
+        self.front_input.extend(bytes)
     }
     fn handle_back_input(&mut self, message: String) {
-        self.input_to_io.push(message);
+        self.back_input.push_back(message);
     }
     fn poll_back_output(&mut self) -> Option<String> {
-        if let Some(pos) = self.input_from_io.iter().position(|&x| x == b'\n') {
-            let message = self.input_from_io.drain(..pos).collect();
-            self.input_from_io.drain(..1);
+        if let Some(pos) = self.front_input.iter().position(|&x| x == b'\n') {
+            let message = self.front_input.drain(..pos).collect();
+            self.front_input.drain(..1);
             Some(String::from_utf8(message).unwrap())
         } else {
             None
         }
     }
     fn poll_front_output(&mut self) -> Option<Vec<u8>> {
-        self.input_to_io.pop().map(|message| {
+        self.back_input.pop_front().map(|message| {
             let mut bytes = message.into_bytes();
             // append new line
             bytes.push(b'\n');
@@ -116,40 +121,39 @@ impl Pipe<Vec<u8>, String, String, Vec<u8>> for BytesToLinesPipe {
 }
 
 #[derive(Default)]
-struct StringToNumberPipe {
-    input_from_io: Vec<String>,
-    output_to_io: Vec<String>,
+struct StringsToNumbersPipe {
+    front_input: VecDeque<String>,
+    front_output: VecDeque<String>,
 }
 
-impl Pipe<String, i32, i32, String> for StringToNumberPipe {
+impl Pipe<String, String, i32, i32> for StringsToNumbersPipe {
     fn handle_front_input(&mut self, message: String) {
-        self.input_from_io.push(message);
+        self.front_input.push_back(message);
     }
-    fn handle_back_input(&mut self, message: i32) {
-        self.output_to_io.push(message.to_string());
+    fn handle_back_input(&mut self, number: i32) {
+        self.front_output.push_back(number.to_string());
     }
     fn poll_back_output(&mut self) -> Option<i32> {
-        self.input_from_io.pop().and_then(|message| {
+        self.front_input.pop_front().and_then(|message| {
             if let Ok(n) = message.parse() {
                 Some(n)
             } else {
-                self.output_to_io.push("Invalid number".to_string());
+                self.front_output
+                    .push_back(format!("Invalid number: {:?}", message));
                 None
             }
         })
     }
     fn poll_front_output(&mut self) -> Option<String> {
-        self.output_to_io.pop()
+        self.front_output.pop_front()
     }
 }
 
-use std::io::{Read, Write};
-
 fn main() {
     let mut bytes_to_numbers_pipe =
-        Glue::new(BytesToLinesPipe::default(), StringToNumberPipe::default());
-    let mut stdin = std::io::stdin().lock();
+        Glue::new(BytesToLinesPipe::default(), StringsToNumbersPipe::default());
 
+    let mut stdin = std::io::stdin().lock();
     loop {
         if let Some(n) = bytes_to_numbers_pipe.poll_back_output() {
             let n = 2 * n;
